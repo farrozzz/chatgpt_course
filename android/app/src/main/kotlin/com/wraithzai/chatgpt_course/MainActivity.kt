06ee48c65cec41ee48a8c9f309ef5554fabc7554
Farros Zain <zain.farros@gmail.com>
2023-04-06 19:41:08 +0800

    progress

---
+++ android/app/src/main/kotlin/com/wraithzai/chatgpt_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wraithzai.chatgpt_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chatgpt_course/constant/constant.dart';
+import 'package:chatgpt_course/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chatgpt_course/screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        appBarTheme: AppBarTheme(
+          color: cardColor,
+        )),
+      home: const ChatScreen(),
+    );
+  }
+}
